57762138eb5036b1b300ef60bd3d2fba8dd77395
+ <Peleanr1989@gmail.com>
2021-11-23 12:42:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
 }
+
+addingEventListener(input);
+
+
+const divThree = document.querySelectorAll('div')[5];
+
+function newEvent() {
+    alert('you ran over me!')
+};
+
+divThree.addEventListener('mouseover', colorEvent);
+
+
+function colorEvent () {
+    const oldBack = divThree.style.backgroundColor = 'black';
+};
+
